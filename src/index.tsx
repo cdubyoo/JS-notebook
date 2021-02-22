a28eaf8bc441df21ef3ae7f21cbda092ecdf19f0
chung <chungwongg@gmail.com>
2021-02-22 00:47:11 -0800

    esbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,44 @@
+import * as esbuild from 'esbuild-wasm'
 import ReactDOM from 'react-dom'
+import { useState, useEffect, useRef } from 'react'
+
 
 const App = () => {
-    return <h1>hi</h1>
+    const ref = useRef<any>()
+    const [input, setInput] = useState('')
+    const [code, setCode] = useState('')
+
+    const startService = async () => {
+        // assign startservice to ref.current to be used outside
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        })
+    }
+
+    // run startservice once every time app renders
+    useEffect(() => {
+        startService()
+    }, [])
+
+    const onClick = () => {
+        // check to make sure service is initialized 
+        if (!ref.current) {
+            return
+        }
+
+        console.log(ref.current)
+    }
+
+    return (
+        <div>
+            <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+        )
 }
 
 ReactDOM.render(

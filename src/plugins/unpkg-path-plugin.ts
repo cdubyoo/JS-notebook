b5f1a9bbbb8b6491a2e7f446bed45f881a2a5a65
cdubyoo <chungwongg@gmail.com>
2021-03-10 09:05:08 -0800

    cache and input bundle

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,8 +1,21 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios'
+import localForage from 'localforage'
+
+const fileCache = localForage.createInstance({
+    name: 'filecache'
+});
+
+(async () => {
+    await fileCache.setItem('color', 'red')
+
+    const color = await fileCache.getItem('color')
+ 
+    console.log(color)
+})()
 
 // override ESBuild to create bundle inside browser
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
     return {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
@@ -25,6 +38,7 @@ export const unpkgPathPlugin = () => {
                     path: `https://unpkg.com/${args.path}`
                 }
             });
+            
 
             build.onLoad({ filter: /.*/ }, async (args: any) => {
                 console.log('onLoad', args);
@@ -33,21 +47,30 @@ export const unpkgPathPlugin = () => {
                     return {
                         loader: 'jsx',
                         // modules inputs
-                        contents: `
-                            const React, { useState } = require('react');
-                            console.log(React, reactDOM);
-                        `,
+                        contents: inputCode
                     };
                 } 
+
+                // check to see if we already fetched this file and if its in the cache
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path) // use type onLoadresult so typescript knows the type
+                // if it is, return it
+                if (cachedResult) {
+                    return cachedResult
+                }
+
                 // get data from url for test pkg
                 const { data, request } = await axios.get(args.path)
-                console.log(request)
-                return {
+                
+                const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
                     contents: data,
                     // find directory of package
                     resolveDir: new URL('./', request.responseURL).pathname
                 }
+                // store response in cache
+                await fileCache.setItem(args.path, result)
+
+                return result
             });
         },
     };

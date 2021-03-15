4190e448d62a81078e17de273ccecde14260241b
chung <chungwongg@gmail.com>
2021-03-15 06:32:29 -0700

    css handling

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,37 +10,65 @@ export const fetchPlugin = (inputCode: string) => {
     return {
         name: 'fetch-plugin',
         setup(build: esbuild.PluginBuild) {
-            
-        build.onLoad({ filter: /.*/ }, async (args: any) => {
-            if (args.path === 'index.js') {
+
+            build.onLoad({ filter: /(^index\.js$)/ }, () => {
                 return {
                     loader: 'jsx',
+                    contents: inputCode,
+                }
+            })
+
+            build.onLoad({ filter: /.css$/ }, async (args: any) => {
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
+        
+                if (cachedResult) {
+                    return cachedResult;
+                }
+                const { data, request } = await axios.get(args.path)
+                const escaped = data
+                    .replace(/\n/g, '')
+                    .replace(/"/g, '\\"')
+                    .replace(/'/g, "\\'")
+                const contents = `
+                    const style = document.createElement('style')
+                    style.innerText = '${escaped}'
+                    document.head.appendChild(style)
+                `
+        
+                const result: esbuild.OnLoadResult = {
+                    loader: 'jsx',
+                    contents,
+                    // find directory of package
+                    resolveDir: new URL('./', request.responseURL).pathname,
+                }
+                // store response in cache
+                await fileCache.setItem(args.path, result)
+        
+                return result
+            });
+
+        
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
+                // check to see if we already fetched this file and if its in the cache
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path) // use type onLoadresult so typescript knows the type
+        
+                if (cachedResult) {
+                    return cachedResult
+                }
+                
+                // get data from url for test pkg
+                const { data, request } = await axios.get(args.path)
+        
+                const result: esbuild.OnLoadResult = {
+                    loader: 'jsx',
                     // modules inputs
-                    contents: inputCode
-                };
-            } 
-
-            // check to see if we already fetched this file and if its in the cache
-            const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path) // use type onLoadresult so typescript knows the type
-            // if it is, return it
-            if (cachedResult) {
-                return cachedResult
-            }
-
-            // get data from url for test pkg
-            const { data, request } = await axios.get(args.path)
-            
-            const result: esbuild.OnLoadResult = {
-                loader: 'jsx',
-                contents: data,
-                // find directory of package
-                resolveDir: new URL('./', request.responseURL).pathname
-            }
-            // store response in cache
-            await fileCache.setItem(args.path, result)
-
-            return result
-        });
+                    contents: data,
+                    resolveDir: new URL('./', request.responseURL).pathname
+                }
+                await fileCache.setItem(args.path, result)
+        
+                return result
+            })
         }
     }
 }

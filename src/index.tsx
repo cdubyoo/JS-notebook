4b1ddbe5d416a6acfe9a8e7e9123afa7590b7b61
chung <chungwongg@gmail.com>
2021-03-29 17:04:19 -0700

    refractor iframe

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,12 @@ import { useState, useEffect, useRef } from 'react'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 import { fetchPlugin } from './plugins/fetch-plugin'
 import CodeEditor from './components/code-editor'
+import Preview from './components/preview'
 
 const App = () => {
     const ref = useRef<any>()
-    const iframe = useRef<any>()
+    const [code, setCode] = useState('')
+
     const [input, setInput] = useState('')
 
     const startService = async () => {
@@ -30,7 +32,7 @@ const App = () => {
             return
         }
 
-        iframe.current.srcdoc = html
+        
 
         // transpile user input
         const result = await ref.current.build({
@@ -48,41 +50,20 @@ const App = () => {
             },
         })
         // transpiled and bundled code
-        // setCode(result.outputFiles[0].text)
-        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+        setCode(result.outputFiles[0].text)
+
     }
-
-    const html = `
-        <html>
-            <head></head>
-            <body>
-                <div id="root"></div>
-                <script>
-                    window.addEventListener('message', (event) => {
-                        try{
-                            eval(event.data)
-                        } catch (err) {
-                            const root = document.querySelector('#root')
-                            root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>'
-                            console.error(err)
-                        }
-                    }, false)
-                </script>
-            </body>
-        </html>
-    `
-
+ 
     return (
         <div>
             <CodeEditor 
                 initialValue="const a = 1"
                 onChange={(value) => setInput(value)}
             />
-            <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <iframe title ="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+            <Preview code={code}/>
         </div>
     )
 }

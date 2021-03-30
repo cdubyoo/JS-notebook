9f36d37f91e68c60ee87031e6dc122f77b998c67
chung <chungwongg@gmail.com>
2021-03-29 17:24:01 -0700

    refractor service build

---
+++ src/bundler/index.ts
@@ -0,0 +1,31 @@
+import * as esbuild from 'esbuild-wasm'
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin'
+import { fetchPlugin } from '../plugins/fetch-plugin'
+
+// bundle our code
+let service: esbuild.Service
+export default async (rawCode: string) => {
+    // check if service has been started, if not start it
+    if (!service) {
+        service = await esbuild.startService({
+            worker: true,
+            wasmURL: 'http://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+        })
+    }
+    const result = await service.build({
+        entryPoints: ['index.js'],
+        bundle: true,
+        write: false,
+        plugins: [
+            unpkgPathPlugin(), fetchPlugin(rawCode)
+        ],
+        
+        // defines for bundling
+        define: {
+            'process.env.NODE_ENV': '"production"',
+            global: 'window'
+        },
+    })
+
+    return result.outputFiles[0].text
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,56 +1,18 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import * as esbuild from 'esbuild-wasm'
 import ReactDOM from 'react-dom'
 import { useState, useEffect, useRef } from 'react'
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
-import { fetchPlugin } from './plugins/fetch-plugin'
 import CodeEditor from './components/code-editor'
 import Preview from './components/preview'
+import bundle from './bundler'
 
 const App = () => {
-    const ref = useRef<any>()
     const [code, setCode] = useState('')
-
     const [input, setInput] = useState('')
 
-    const startService = async () => {
-        // assign startservice to ref.current to be used outside
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: 'http://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        })
-    }
-
-    // run startservice once every time app renders
-    useEffect(() => {
-        startService()
-    }, [])
-
     const onClick = async () => {
-        // check to make sure service is initialized 
-        if (!ref.current) {
-            return
-        }
-
-        
-
-        // transpile user input
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [
-                unpkgPathPlugin(), fetchPlugin(input)
-            ],
-            
-            // defines for bundling
-            define: {
-                'process.env.NODE_ENV': '"production"',
-                global: 'window'
-            },
-        })
+        const output = await bundle(input)
         // transpiled and bundled code
-        setCode(result.outputFiles[0].text)
+        setCode(output)
 
     }
  

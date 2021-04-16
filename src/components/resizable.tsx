1c0aff19f86f514affa6e1f001f052e60585a78b
chung <chungwongg@gmail.com>
2021-04-15 17:56:15 -0700

    resizable

---
+++ src/components/resizable.tsx
@@ -0,0 +1,19 @@
+import './resizable.css'
+import { ResizableBox } from 'react-resizable'
+
+interface ResizableProps {
+    direction: 'horizontal' | 'vertical'
+
+}
+
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+    return (
+        <ResizableBox 
+            height={300} width={300} resizeHandles={['s']}
+        >
+            {children}
+        </ResizableBox>
+    )
+}
+
+export default Resizable

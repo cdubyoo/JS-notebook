67af9cb275b2f992f21ff5a623b9f16966e4fca8
chung <chungwongg@gmail.com>
2021-04-27 11:41:53 -0700

    resizable css

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -8,7 +8,9 @@ interface PreviewProps {
 
 const html = `
         <html>
-            <head></head>
+            <head>
+                <style>html {background-color: white;} </style>
+            </head>
             <body>
                 <div id="root"></div>
                 <script>

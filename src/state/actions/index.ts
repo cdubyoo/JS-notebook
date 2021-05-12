9ecc7db134c529c51fa95c34fa95cd05a5bfc1b0
chung <chungwongg@gmail.com>
2021-05-11 17:59:42 -0700

    action types

---
+++ src/state/actions/index.ts
@@ -0,0 +1,34 @@
+ import { ActionType } from '../action-types'
+
+// define interface for each action
+
+interface MoveCellAction {
+    type: ActionType.MOVE_CELL
+    payload: {
+        id: string
+        direction: 'up' | 'down'
+    }
+}
+
+interface DeleteCellAction {
+    type: ActionType.DELETE_CELL
+    payload: string // just need id to delete
+}
+
+interface InsertCellBeforeAction {
+    type: ActionType.INSERT_CELL_BEFORE
+    payload: {
+        id: string
+        type: 'code' | 'text'
+    }
+}
+
+interface UpdateCellAction {
+    type: ActionType.UPDATE_CELL
+    payload: {
+        id: string
+        content: string
+    }
+}
+
+export type Action = MoveCellAction | DeleteCellAction | InsertCellBeforeAction | UpdateCellAction

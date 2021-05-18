60087453a3b5953bcb95b9ad2d3c1187626d7358
chung <chungwongg@gmail.com>
2021-05-18 00:42:37 -0700

    cell reducer cases

--- src/state/cell.ts
+++ src/state/cell.ts
@@ -1,5 +1,5 @@
 export type CellTypes = 'code' | 'test'
-
+// this is what a cell is
 export interface Cell {
     id: string
     type: CellTypes
--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -27,14 +27,45 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
             state.data[id].content = content
             return
         case ActionType.DELETE_CELL:
+            delete state.data[action.payload]
+            state.order = state.order.filter((id) => id !== action.payload)
             return state
         case ActionType.MOVE_CELL:
-            return state
+            const { direction } = action.payload
+            const index = state.order.findIndex((id) => id === action.payload.id)
+            // if direction is up and index is 1 then target is 0
+            const targetIndex = direction === 'up' ? index - 1 : index + 1
+
+            if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+                return
+            }
+            state.order[index] = state.order[targetIndex]
+            state.order[targetIndex] = action.payload.id
+            return 
         case ActionType.INSERT_CELL_BEFORE:
+            const cell: Cell = {
+                content: '',
+                type: action.payload.type,
+                id: randomId()
+            }
+
+            state.data[cell.id] = cell
+            const foundIndex = state.order.findIndex(id => id === action.payload.id)
+
+            if (foundIndex < 0) {
+                state.order.push(cell.id)
+            } else {
+                state.order.splice(foundIndex, 0, cell.id)
+            }
+            
             return state
         default:
             return state
     }
 })
+
+const randomId = () => {
+    return Math.random().toString(36).substring(2, 5)
+}
 
 export default reducer

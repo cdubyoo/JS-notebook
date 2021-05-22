4c12ab8db104c0a038dc469799e59a8f1b2f238b
Chung <chungwongg@gmail.com>
2021-05-21 19:16:07 -0700

    type selector

---
+++ src/components/cell-list.tsx
@@ -0,0 +1,9 @@
+import { useTypedSelector } from '../hooks/use-typed-selector'
+
+const CellList: React.FC = () => {
+    useTypedSelector((state) => state)
+
+    return <div>Cell List</div>
+}
+
+export default CellList

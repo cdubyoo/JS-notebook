a0197dd0c883e7e507040df82013e7060ddb9dba
Chung <chungwongg@gmail.com>
2021-05-22 08:12:25 -0700

    celllist

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,10 +1,11 @@
 import { useTypedSelector } from '../hooks/use-typed-selector'
 
 const CellList: React.FC = () => {
-    const cells = useTypedSelector(({ cells: { order, data } }) =>
-    order.map((id) => data[id])
-    );
-
+    const cells = useTypedSelector(({ cells: { order, data } }) => {
+        return order.map((id) => {
+            return data
+        })
+    })
     return <div>Cell List</div>
 }
 

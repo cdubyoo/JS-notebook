caa31586e62e7191484b2d55b1a6d78c4709b0c2
cdubyoo <chungwongg@gmail.com>
2021-07-10 06:21:07 -0700

    serve command

--- packages/cli/dist/commands/serve.ts
+++ packages/cli/dist/commands/serve.ts
@@ -1,8 +1,9 @@
 import { Command } from 'commander'
 
 export const serveCommand = new Command()
-    .command('serve')
+    .command('serve [filename]')
     .description('Open a file for editing')
+    .option('-p, --port <number>', 'port to run server on', '4005')
     .action(() => {
         console.log('Getting ready to serve a file')
     })

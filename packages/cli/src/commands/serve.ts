7258f5549ff5fa92f471513c6d846ca9930ded33
cdubyoo <chungwongg@gmail.com>
2021-07-12 10:37:29 -0700

    serve command error handling

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -6,7 +6,11 @@ export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
-  .action((filename = 'notebook.js', options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(options.port), path.basename(filename), dir);
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+    } catch (err) {
+      console.log('Heres the problem', err.message)
+    }
   });

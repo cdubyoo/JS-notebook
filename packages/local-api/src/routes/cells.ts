15c0c2bca986c53e1b2d03183c0e54320a857d2d
cdubyoo <chungwongg@gmail.com>
2021-07-14 13:41:58 -0700

    cell router

---
+++ packages/local-api/src/routes/cells.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+export const createCellsRouter = (filename: string, dir: string) => {
+    const router = express.Router()
+    router.get('/cells', async (req, res) => {
+
+    })
+    
+    router.post('/cells', async (req, res) => {
+        
+    })
+}
+

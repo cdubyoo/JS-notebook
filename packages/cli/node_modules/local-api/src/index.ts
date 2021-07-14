39cf9604e5117151258f4c38397f147017319850
cdubyoo <chungwongg@gmail.com>
2021-07-14 05:19:07 -0700

    condition useproxy

--- packages/cli/node_modules/local-api/src/index.ts
+++ packages/cli/node_modules/local-api/src/index.ts
@@ -1,17 +1,20 @@
 import express from 'express'
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path'
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (port: number, filename: string, dir: string, useProxy: boolean) => {
   const app = express()
 
-  app.use(express.static('../../local-client/build'))
-
-  // app.use(createProxyMiddleware({
-  //   target: 'https://localhost:3000',
-  //   ws: true,
-  //   logLevel: 'silent'
-  // }))
-  
+  if (useProxy) {
+    app.use(createProxyMiddleware({
+      target: 'https://localhost:3000',
+      ws: true,
+      logLevel: 'silent'
+    }))
+  } else {
+    const packagePath = require.resolve('local-client/build/index.html')
+    app.use(express.static(path.dirname(packagePath)))
+  }
   // create own promise to either resolve or reject
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error',reject)
--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,14 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
-var serve = function (port, filename, dir) {
+var http_proxy_middleware_1 = require("http-proxy-middleware");
+var path_1 = __importDefault(require("path"));
+var serve = function (port, filename, dir, useProxy) {
     var app = express_1.default();
-    app.use(express_1.default.static('../../local-client/build'));
-    // app.use(createProxyMiddleware({
-    //   target: 'https://localhost:3000',
-    //   ws: true,
-    //   logLevel: 'silent'
-    // }))
+    if (useProxy) {
+        app.use(http_proxy_middleware_1.createProxyMiddleware({
+            target: 'https://localhost:3000',
+            ws: true,
+            logLevel: 'silent'
+        }));
+    }
+    else {
+        var packagePath = require.resolve('local-client/build/index.html');
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
     // create own promise to either resolve or reject
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on('error', reject);

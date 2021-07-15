7b50089226770d58fb5a07d0f8f7eda66c096cae
cdubyoo <chungwongg@gmail.com>
2021-07-15 15:20:39 -0700

    fetch cell action creator

--- packages/local-api/node_modules/local-client/src/state/action-creators/index.ts
+++ packages/local-api/node_modules/local-client/src/state/action-creators/index.ts
@@ -1,4 +1,5 @@
 import {  Dispatch } from 'redux'
+import axios from 'axios'
 import { ActionType } from '../action-types'
 import { 
     UpdateCellAction, 
@@ -8,7 +9,7 @@ import {
     Direction,
     Action
 } from '../actions'
-import { CellTypes } from '../cell'
+import { Cell, CellTypes } from '../cell'
 import bundle from '../../bundler'
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
@@ -69,5 +70,25 @@ export const createBundle = (cellId: string, input: string) => {
                 }
             }
         })
+    }
+}
+
+export const fetchCells = () => {
+    return async (dispatch: Dispatch<Action>) => {
+        dispatch({ type: ActionType.FETCH_CELLS})
+
+        try {
+            const { data }: { data: Cell[]} = await axios.get('/cells')
+            
+            dispatch({
+                type: ActionType.FETCH_CELLS_COMPLETE,
+                payload: data
+            })
+        } catch (err) {
+            dispatch({
+                type: ActionType.FETCH_CELLS_ERROR,
+                payload: err.message
+            })
+        }
     }
 }
